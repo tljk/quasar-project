bd3fb39d2177d02f040ef2a10ef79ba90f3ba68a
tljk <tanglinchuanz@163.com>
2024-09-15 01:31:32 -0400

    [general] support webview update in capacitor android

--- src-capacitor/android/app/src/main/java/org/capacitor/quasar/app/MainActivity.java
+++ src-capacitor/android/app/src/main/java/org/capacitor/quasar/app/MainActivity.java
@@ -1,5 +1,17 @@
 package org.capacitor.quasar.app;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import com.github.tljk.CapacitorWebviewUpdatePlugin;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    CapacitorWebviewUpdatePlugin capacitorWebviewUpdatePlugin = new CapacitorWebviewUpdatePlugin();
+    capacitorWebviewUpdatePlugin.apply(getApplicationContext());
+
+    super.onCreate(savedInstanceState);
+  }
+}
